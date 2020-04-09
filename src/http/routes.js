b5ab2c6d144726c73b745494b343003389736aa1
Leonardo Fernandes Machado da Silva <loadfms@gmail.com>
2020-04-09 14:07:07 -0300

    test

--- src/http/routes.js
+++ src/http/routes.js
@@ -1,11 +1,4 @@
-const db = require('../services/pg')
-const registerTaskRoutes = require('./task/task')
-const registerAuthRoutes = require('./auth/auth')
-const registerEmployeeRoutes = require('./employee/employee')
-const registerUserRoutes = require('./user/user')
-const registerTaskEmloyeeRoutes = require('./taskemployee/taskemployee')
-const registerCustomerRoutes = require('./customer/customer')
-const registerAppointmentRoutes = require('./appointment/appointment')
+
 const registerHealthRoutes = require('./health/health')
 
 const routes = (server) => {
@@ -14,13 +7,13 @@ const routes = (server) => {
     })
 
     registerHealthRoutes({server})
-    registerTaskRoutes({server, db})
-    registerAuthRoutes({server, db})
-    registerEmployeeRoutes({server, db})
-    registerUserRoutes({server, db})
-    registerTaskEmloyeeRoutes({server, db})
-    registerCustomerRoutes({server, db})
-    registerAppointmentRoutes({server, db})
+    // registerTaskRoutes({server, db})
+    // registerAuthRoutes({server, db})
+    // registerEmployeeRoutes({server, db})
+    // registerUserRoutes({server, db})
+    // registerTaskEmloyeeRoutes({server, db})
+    // registerCustomerRoutes({server, db})
+    // registerAppointmentRoutes({server, db})
 }
 
 module.exports = routes
